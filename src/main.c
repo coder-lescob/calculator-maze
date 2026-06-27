d8dbedd5e272841f6c80ee58d819a222151ba685
coder-lescob <gabriel.lescob@outlook.com>
2026-06-27 13:06:28 +0200

    *ok I just needed to poll the keyboard and tweak now the simulator works

--- src/main.c
+++ src/main.c
@@ -3,7 +3,17 @@
 const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Maze";
 const uint32_t eadk_api_level  __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
-int main(int argc, char *argv[]) {
+int main(void) {
     eadk_display_draw_string("Hello, world!", (eadk_point_t){0, 0}, true, eadk_color_black, eadk_color_white);
-    eadk_timing_msleep(3000);
+    
+    while (true) {
+        eadk_keyboard_state_t keyboad = eadk_keyboard_scan();
+
+        if (eadk_keyboard_key_down(keyboad, eadk_key_back)) {
+            break;
+        }
+
+        // ~60 FPS
+        eadk_timing_msleep(16);
+    }
 }
