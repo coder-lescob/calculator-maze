69c294bf46351cf588091093c2fadb0a71825169
coder-lescob <gabriel.lescob@outlook.com>
2026-06-27 12:29:05 +0200

    * indent 4 is better

--- src/main.c
+++ src/main.c
@@ -4,6 +4,6 @@ const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "
 const uint32_t eadk_api_level  __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
 int main(int argc, char *argv[]) {
-  eadk_display_draw_string("Hello, world!", (eadk_point_t){0, 0}, true, eadk_color_black, eadk_color_white);
-  eadk_timing_msleep(3000);
+    eadk_display_draw_string("Hello, world!", (eadk_point_t){0, 0}, true, eadk_color_black, eadk_color_white);
+    eadk_timing_msleep(3000);
 }
