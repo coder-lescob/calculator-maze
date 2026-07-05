fb5216701b07695c34d11b081cfd22cf45cccd7f
coder-lescob <gabriel.lescob@outlook.com>
2026-07-05 16:19:28 +0200

    * oops post processing the maze is actually necessairy

--- src/maze.c
+++ src/maze.c
@@ -117,10 +117,11 @@ Maze generate_maze(uint16_t width, uint16_t height)
     } 
     while (current_cell != 0);
 
-    // Path done on square 0
-    maze.tiles[0] = PATH_DONE;
+    // remove any none wall with PATH_DONE
+    for (int i = 0; i < maze.width * maze.height; i++)
+        if (maze.tiles[i] != WALL) maze.tiles[i] = PATH_DONE;
 
-    // print_maze(&maze);
+    print_maze(&maze);
 
     return maze;
 }
