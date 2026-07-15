e6f0b53c040bc7c39feb92d399258abb6473930a
coder-lescob <gabriel.lescob@outlook.com>
2026-07-15 11:26:01 +0200

    * show the version

--- src/main.c
+++ src/main.c
@@ -7,9 +7,9 @@
 #include "maze.h"
 #include "player.h"
 
-#define VERSION ("0.4.0")
+#define VERSION "0.4.0"
 
-const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Maze";
+const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Maze " VERSION;
 const uint32_t eadk_api_level  __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
 int main(void) {
