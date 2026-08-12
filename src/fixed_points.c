009b3369bbfd9c08e0edc48023bd2af8ff0ed859
coder-lescob <gabriel.lescob@outlook.com>
2026-08-12 08:46:00 +0200

    * please avoid hardcoding constants in the future

--- src/fixed_points.c
+++ src/fixed_points.c
@@ -4,11 +4,11 @@
 #include <string.h>
 
 inline fixed16_t float_to_fixed16(float x) {
-    return (fixed16_t)(x * 65536.0f);
+    return (fixed16_t)(x * (UINT16_MAX - 1));
 }
 
 inline float fixed16_to_float(fixed16_t x) {
-    return (float)x / 65535.0f;
+    return (float)x / (UINT16_MAX - 1);
 }
 
 inline fixed16_t fixed16_mul(fixed16_t a, fixed16_t b) {
